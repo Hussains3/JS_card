327741a83e732feca831913ddfcb371b387013a8
Husssains3 <hussainasabbir3@gmail.com>
2020-11-05 14:55:19 +0600

    Udemy course lesson 43

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var myDatabase = [
+    {name:'Sabbir hussain',email:'email@mail.com',age:25},
+    {name:'Tanvir hussain',email:'alom@mail.com',age:24},
+    {name:'Jenny',email:'bitic@mail.com',age:23},
+]
